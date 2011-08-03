43b2e537c737583c7205c622386cdf04ee5f6c20
Gabe Coyne <gabe@killitcreative.com>
2011-08-03 12:15:02 -0700

    rescue redirects that aren't found with 404

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,5 +1,5 @@
 class RedirectsController < ApplicationController
   def show
-    return redirect_to "/"+Redirect.find_by_source(params[:path]).destination
+    return redirect_to "/"+Redirect.find_by_source(params[:path]).destination rescue header("HTTP/1.0 404 Not Found");
   end
 end
